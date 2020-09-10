00bc27e2bf20848231bad331e3203b5c120bf9e4
katrina <katrina.nov13@gmail.com>
2020-09-10 20:01:47 +0300

    init: test datafolks

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$( document ).ready(function() {
+  // modal
+  $('#modal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget);
+    var title = button.children('.card-img-overlay').children('.card-title').text();
+    var descr = button.children('.card__description').text();
+    var imgSrc = button.children('.card-img').attr('src');
+    var imgAlt = button.children('.card-img').attr('alt');
+    var modal = $(this);
+    modal.find('.modal-title').text(title);
+    modal.find('.modal-body .description').text(descr);
+    modal.find('.modal-body img').attr({
+      src: imgSrc,
+      alt: imgAlt
+        });
+  })
+  // collapse
+  $('#collapse').on('show.bs.collapse', function () {
+    $('.project__btn').text('Скрыть');
+  });
+  $('#collapse').on('hidden.bs.collapse', function () {
+    $('.project__btn').text('Звгрузить еще');
+  });
+});
